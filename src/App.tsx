81afb4f5eb7394e9264309cae03bdf3fe18a8acb
anisg <anisoupe@gmail.com>
2023-08-05 14:25:44 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React, { useState } from "react";
+import { MainPage } from "./components/live/mainPage/MainPage";
 
 function App() {
+  const [dogImage, setDogImage] = useState<string | null>(null);
+
+  function generateImage() {
+    console.log("click");
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(data => setDogImage(data.message))
+  }
 
   return (
-    <>todo</>
+    <MainPage frames={{
+      mainPage: {
+        className: 'w-full h-full'
+      },
+      cuteDogPictureGenerator: {
+        onClick: generateImage,
+        ...(dogImage && {
+          children: <img src={dogImage} />
+        })
+      },
+      buttonDownload: {
+        onClick: generateImage,
+      }
+    }} />
   );
 }
 
