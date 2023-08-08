c3a245446abdc9e36636034108927c16303e12ce
anisg <anisoupe@gmail.com>
2023-08-08 18:57:05 +0200

    [LiveDesign] Updated 1 component

--- src/components/live/mainPage/RenderMainPage.tsx
+++ src/components/live/mainPage/RenderMainPage.tsx
@@ -48,11 +48,11 @@ export function RenderMainPage(props: RenderMainPageProps) {
         </Div>
       </Div>
       <Div {...frames["frame5"]} className={cls(frames, "frame5")} style={{gap: 30, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flexShrink: 0, alignSelf: 'stretch', objectFit: 'cover', position: 'relative', padding: '0px 0px 0px 0px', opacity: 1, display: 'flex', ...frames["frame5"]}}>
-        <Button frames={{"button":{"style":"style={{flexShrink: 0, display: " flex'}, ...frames["button"]}}},"title":{"className":"title","style":"style="{{}}&quot;,&quot;content&quot;:&quot;Download" Image"}}' />
+        <Button frames={{"button":{"style":"flex-shrink: 0; display: flex"},"title":{"className":"title","style":"","content":"Download Image"}, ...frames["button"]}} />
       </Div>
     </Div>
   </Div>
-  <BadgeLiveDesign frames={{"badgeLiveDesign":{"style":"style={{flexShrink: 0, position: " absolute', bottom: 17, right: 27, display: 'flex'}, ...frames["badgeLiveDesign"]}}},"madeOn":{"className":"madeOn","style":"style="{{}}&quot;},&quot;liveDesign&quot;:{&quot;className&quot;:&quot;liveDesign&quot;,&quot;style&quot;:&quot;style={{}}&quot;}}'" />
+  <BadgeLiveDesign frames={{"badgeLiveDesign":{"style":"flex-shrink: 0; position: absolute; bottom: 17px; right: 27px; display: flex"},"madeOn":{"className":"madeOn","style":""},"liveDesign":{"className":"liveDesign","style":""}, ...frames["badgeLiveDesign"]}} />
 </Div>
     );
 }
