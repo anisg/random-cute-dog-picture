11f9d62926a9e3fe2c9b9c4da051d86aacf7d8d6
anisg <anisoupe@gmail.com>
2023-08-08 19:45:24 +0200

    [LiveDesign] Updated 2 components

--- src/components/live/button/RenderButton.tsx
+++ src/components/live/button/RenderButton.tsx
@@ -14,7 +14,7 @@ export type RenderButtonProps = {
 export function RenderButton(props: RenderButtonProps) {
     const { frames = {}, ...restProps } = props;
     return (
-        <Div {...frames["button"]} className={cls(frames, "button")} style={{gap: 10, flexDirection: 'row', width: 'fit-content', alignItems: 'flex-start', overflow: 'hidden', position: 'relative', border: '1px  SOLID #c73d3d', borderRadius: 3, padding: '5px 11px 5px 11px', backgroundColor: '#db4242', opacity: 1, display: 'flex', ...frames["button"]?.style}}><Span {...frames["title"]} className={cls(frames, "title")} style={{fontFamily: 'Inter', fontSize: 18, fontWeight: 600, color: '#ffffff', textAlign: 'left', display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', flexShrink: 0, position: 'relative', ...frames["title"]?.style}}>title</Span>
+        <Div {...frames["button"]} className={cls(frames, "button")} style={{gap: 10, flexDirection: 'row', width: 'fit-content', alignItems: 'flex-start', overflow: 'hidden', position: 'relative', border: '1px  SOLID #c73d3d', borderRadius: 3, padding: '5px 11px 5px 11px', backgroundColor: '#e3a8a8', opacity: 1, display: 'flex', ...frames["button"]?.style}}><Span {...frames["title"]} className={cls(frames, "title")} style={{fontFamily: 'Inter', fontSize: 18, fontWeight: 600, color: '#ffffff', textAlign: 'left', display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', flexShrink: 0, position: 'relative', ...frames["title"]?.style}}>title</Span>
 </Div>
     );
 }
